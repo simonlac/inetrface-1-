c834fc4afe543f0b7e713519c7bb0f10b928620e
simonlac <101816652+simonlac@users.noreply.github.com>
2022-10-26 10:49:48 -0400

    Add files via upload

---
+++ Exemple_Interface (1)/Exemple_Interface/Exemple_Interface/interface.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemple_Interface
+{
+    class Program
+    {
+        interface IAnimal
+        {
+            string Name { get; }
+            void Move();
+        }
+
+        class Dog : IAnimal
+        {
+            private string name;
+            
+            // On implémente la propriét Name ici.
+            public string Name
+            {
+                get {return name;}
+            }
+            // Le constructeur de la classe Dog
+            public Dog(string p_name)
+            {
+                name = p_name;
+            }
+
+            // On implémente la méthode Move ici.
+             public void Move()
+            {
+                Console.WriteLine("{0} bouge.", name);
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Dog d1 = new Dog("Bob");
+            d1.Move();
+            // Stocker un objet de type Dog dans un autre type : IAnimal
+            IAnimal d2 = new Dog("Jon");
+            d2.Move();
+            Console.ReadKey();
+        }
+
+    }
+}
